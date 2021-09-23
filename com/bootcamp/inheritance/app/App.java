3cf9cf85c6e1eeb20183ea31e569542df675ef7d
blackhathacker101 <justindouglas1738@gmail.com>
2021-09-23 16:30:47 -0300

    update project

--- com/bootcamp/inheritance/app/App.java
+++ com/bootcamp/inheritance/app/App.java
@@ -1,9 +1,12 @@
 package com.bootcamp.inheritance.app;
 
+//FIXME make sure to ask the user for input on which calculator ton show based on on the given options
+//FIXME make classes for Scientific, Graphing, Printing, Online calculators
 public class App {
 
 	public static void main(String[] args) {
-		
+		Calculator newCalculator = new Calculator();
+		newCalculator.addition();
 	}
 
 }
---
+++ com/bootcamp/inheritance/app/Calculator.java
@@ -0,0 +1,26 @@
+package com.bootcamp.inheritance.app;
+
+/*
+ * @Justin
+ * 
+ * your basic calculator with standard functionability attributes
+ */
+
+public class Calculator {
+
+	public void addition() {
+		System.out.println("adds");
+	}
+	
+	public void subtraction() {
+		System.out.println("subtracts");
+	}
+	
+	public void multiplication() {
+		System.out.println("multiplies");
+	}
+	
+	public void division() {
+		System.out.println("divides");
+	}
+}
